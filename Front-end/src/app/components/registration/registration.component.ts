32006dc1141f7f3544194bd745c938c63a08b10d
VanessaMosquera <jvmosquera47@misena.edu.co>
2023-02-22 15:26:45 -0500

    creacion footer vanessaMosquera

--- Front-end/src/app/components/registration/registration.component.ts
+++ Front-end/src/app/components/registration/registration.component.ts
@@ -2,54 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClientsService } from '../../services/clients.service';
 
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  form!: FormGroup;
+  form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder, 
-    private clients: ClientsService
-  ) { }
+  constructor(private fb: FormBuilder, private clients: ClientsService) {
+    this.form = this.fb.group({
+      names: ['', Validators.required],
+      lastnames: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
-    localStorage.setItem("token", "abcdefghijklmnopqrstuvwxyz");
-    this.form = this.fb.group({
-    name: ['', Validators.required],
-    lastname: ['', Validators.required],
-    // phone: ['', Validators.required],
-    email: ['', Validators.required],
-    password: ['', Validators.required],
-
-  });
-
-}
-onSubmit(){
-  if (this.form.valid) {
-    this.clients.postRequest("http://localhost:10101/register",
-    {
-      name: this.form.value.name,
-      lastname: this.form.value.lastname,
-     // phone: this.form.value.phone,
-      email: this.form.value.email,
-      password: this.form.value.password
-    }, undefined, {"Authorization": `Bearer ${localStorage.getItem("token")}`}).subscribe(
-      ((response: any) => {
-        console.log(response);   
-      }), 
-      ((error: any) => {
-        console.log("error");
-        
-      })
-    )
-
-  }else{
-    console.log("Verifique sus datos");
+    localStorage.setItem('token', 'abcdefghijklmnopqrstuvwxyz');
   }
-}
 
-
+  onSubmit() {
+    if (this.form.valid) {
+      this.clients
+        .postRequest(
+          'http://localhost:10101/register',
+          {
+            name: this.form.value.names,
+            lastname: this.form.value.lastnames,
+            phone: this.form.value.phone,
+            email: this.form.value.email,
+            password: this.form.value.password,
+          },
+          undefined,
+          { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        )
+        .subscribe(
+          (response: any) => {
+            console.log(response);
+          },
+          (error: any) => {
+            console.log('error');
+          }
+        );
+    } else {
+      console.log('Verifique sus datos');
+    }
+  }
 }
--- Front-end/src/app/services/clients.service.ts
+++ Front-end/src/app/services/clients.service.ts
@@ -7,52 +7,53 @@ import { HttpClient, HttpParams, HttpHeaders } from "@angular/common/http";
 export class ClientsService {
 
   constructor(private http: HttpClient) {}
-    getRequest(route: string, queries?: any, headers?:any) {
-      let reqHeaders = new HttpHeaders();
-      let reqParams = new HttpParams();
-  
-      if (Headers) {
-        Object.getOwnPropertyNames(headers).forEach((key) => {
-          reqHeaders = reqHeaders.set(key, headers[key]);
-        });
-      }
-  
-      if (queries) {
-        Object.getOwnPropertyNames(queries).forEach((key) => {
-          reqParams = reqParams.set(key, queries[key]);
-        });
-      }
-  
-      return this.http.get(route, {
-        headers: reqHeaders,
-        params: reqParams,
-        responseType: "json",
-        withCredentials: true,
+
+  getRequest(route: string, queries?: Record<string, string>, headers?: Record<string, string>) {
+    let reqHeaders = new HttpHeaders();
+    let reqParams = new HttpParams();
+
+    if (headers) {
+      Object.getOwnPropertyNames(headers).forEach((key) => {
+        reqHeaders = reqHeaders.set(key, headers[key]);
       });
-   }
-  postRequest(route: string, data?: Object, queries?: any, headers?: any){
-  let reqHeaders = new HttpHeaders();
-  let reqParams = new HttpParams();
+    }
 
-  if (Headers) {
-    Object.getOwnPropertyNames(Headers).forEach((key) => {
-      reqHeaders = reqHeaders.set(key, headers[key]);
+    if (queries) {
+      Object.getOwnPropertyNames(queries).forEach((key) => {
+        reqParams = reqParams.set(key, queries[key]);
+      });
+    }
+
+    return this.http.get(route, {
+      headers: reqHeaders,
+      params: reqParams,
+      responseType: "json",
+      withCredentials: true,
     });
   }
 
-  if (queries) {
-    Object.getOwnPropertyNames(queries).forEach((key) => {
-      reqParams = reqParams.set(key, queries[key]);
-    });
-  }
 
-  return this.http.post(route, data, {
-    headers: reqHeaders,
-    params: reqParams,
-    responseType: "json",
-    withCredentials: true,
+  postRequest(route: string, data?: any, queries?: Record<string, string>, headers?: Record<string, string>) {
+    let reqHeaders = new HttpHeaders();
+    let reqParams = new HttpParams();
+
+    if (headers) {
+      Object.getOwnPropertyNames(headers).forEach((key) => {
+        reqHeaders = reqHeaders.set(key, headers[key]);
+      });
+    }
+
+    if (queries) {
+      Object.getOwnPropertyNames(queries).forEach((key) => {
+        reqParams = reqParams.set(key, queries[key]);
+      });
+    }
+
+    return this.http.post(route, data, {
+      headers: reqHeaders,
+      params: reqParams,
+      responseType: "json",
+      withCredentials: true,
     });
   }
 }
-
-
