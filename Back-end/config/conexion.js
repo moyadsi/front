d2ce4a57e85bed9065f835e5dc327568833d05d3
cohnante <gcbolaos3@misena.edu.co>
2023-03-02 20:11:58 -0500

    No Entendi este error

--- Back-end/config/conexion.js
+++ Back-end/config/conexion.js
@@ -1,8 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+require('dotenv').config()
+
 const conexion = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
+    host:'localhost'||process.env.hostNumber,
+    user:process.env.user||'root',
+    password:'' || process.env.password,
     database:'MetAnimation'
 });
 
--- Back-end/index.js
+++ Back-end/index.js
@@ -1,9 +1,9 @@
 const morgan = require('morgan')
-require('./config/conexion');
 const express = require('express');
 const cors = require('cors');
-const { prependListener } = require('./config/conexion');
-const port = (process.env.port || 3000);
+require('dotenv').config()
+
+const port = process.env.port || 3000 ;
 
 // Express
 const app = express();
