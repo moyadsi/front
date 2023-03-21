1882c9d2ad21299ba587fe51fedb83b39563d234
VanessaMosquera <jvmosquera47@misena.edu.co>
2023-03-20 21:56:17 -0500

    actualizacion landing vm

---
+++ Front-end/src/app/components/best-meta/best-meta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-best-meta',
+  templateUrl: './best-meta.component.html',
+  styleUrls: ['./best-meta.component.css']
+})
+export class BestMetaComponent implements OnInit {
+selectedValue: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
