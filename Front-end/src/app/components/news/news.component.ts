f566457cf33fd1be4277b12ca8a55891b8aa9d96
Cristofert Ocampo <ocampocristofert@gmail.com>
2023-04-28 20:32:11 -0500

    Seccion noticias terminada

--- Front-end/src/app/components/news/news.component.ts
+++ Front-end/src/app/components/news/news.component.ts
@@ -4,14 +4,17 @@ import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-news',
-  templateUrl:'./news.component.html',
+  templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 
 export class NewsComponent implements OnInit {
-  public dataNews: any = {} ;
+  public dataNews: any = {};
   public news: any[] = [];
-  public podcast: any[] = [];
+  public podcast: any = {};
+  public podcastItems: any = {};
+  public itemsPodcast: any = {};
+
   constructor(private apiService: ApiService, private apiPodcast: ApiPodcastService) { }
 
   ngOnInit(): void {
@@ -19,19 +22,21 @@ export class NewsComponent implements OnInit {
     this.traerPodcast()
   }
 
-  llenarData(){
-    this.apiService.getData().subscribe(dataNews =>{
+  llenarData() {
+    this.apiService.getData().subscribe(dataNews => {
       this.dataNews = dataNews;
       this.news = this.dataNews.articles
     })
   }
 
-  traerPodcast(){
-    this.apiPodcast.getDataPodcast().subscribe(podcast =>{
+  traerPodcast() {
+    this.apiPodcast.getDataPodcast().subscribe(podcast => {
       this.podcast = podcast;
-      console.log(podcast)
-      // this.podcast = this.data.articles
-      // console.log(this.podcast)
+      this.podcastItems = this.podcast.data.podcastUnionV2.episodesV2.items
+  
     })
   }
+
+
+
 }
