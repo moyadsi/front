d8182a8f779f3d5f42eb026383290b6369140370
VanessaMosquera <jvmosquera47@misena.edu.co>
2023-06-06 16:44:21 -0500

    Creacion servicio cursos para pintar informacion en la vista -VM

--- Front-end/src/app/components/favorite-courses/favorite-courses.component.ts
+++ Front-end/src/app/components/favorite-courses/favorite-courses.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import axios from 'axios';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { CoursesService } from 'src/app/services/courses.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-favorite-courses',
@@ -20,17 +17,17 @@ export class FavoriteCoursesComponent implements OnInit {
   nombreProfeSeleccionado: any;
   experienciaSeleccionada: any;
   profesionSeleccionada: any;
+  categorias: { id: any; nombre: any; }[] | undefined;
+  urlSeleccionada: string | undefined;
 
+  constructor(private cursosService: CoursesService, private sanitizer: DomSanitizer) {}
 
-  constructor( private http: HttpClient, private sanitizer: DomSanitizer) { }
-
-  /*MOSTRAR CATEGORIAS*/
   obtenerCategorias() {
-    this.http.get<any[]>('http://localhost:5000/api/category').subscribe(
+    this.cursosService.obtenerCategorias().subscribe(
       (response) => {
         this.categorias = response.map(({ Id_Category: id, NameCategory: nombre }) => ({ id, nombre }));
         this.nombreCategoria = this.categorias[0]?.nombre; // Asignar el valor del primer elemento de la categoría al nombreCategoria
-      console.log(this.categorias)
+        console.log(this.categorias)
       },
       (error) => {
         console.error(error);
@@ -45,97 +42,79 @@ public videoUrl: SafeResourceUrl | undefined;
     this.nombreCategoria = categoria.nombre;
     this.idCategoriaSeleccionada = categoria.id;
   }
-  /*OBTENER CURSO DESCRIPCION Y URL DE VIDEO SEGUN LA CATEGORIA*/
+  obtenerCursos(idCategoria: string) {
+    this.cursosService.obtenerCursos(idCategoria).subscribe(
+      (response) => {
+        this.cursos = response.filter(curso => curso.Id=== idCategoria).map(curso => {
+          return {
+            id: curso.Id,
+            url: curso.Url,
+            descripcion: curso.DescriptionCourse,
+            idteacher: curso.IdTeacher
+          };
+        });
   
-// Declara una variable de clase para almacenar la URL
-// Declara una variable de clase para almacenar la URL
-public urlSeleccionada: string = '';
-
-obtenerCursos(idCategoria: string) {
-  const url = `http://localhost:5000/api/course/?categoria=${idCategoria}`;
-
-  this.http.get<any[]>(url).subscribe(
-    (response) => {
-      this.cursos = response.filter(curso => curso.Id=== idCategoria).map(curso => {
-        return {
-          id: curso.Id,
-          url: curso.Url,
-          descripcion: curso.DescriptionCourse,
-          idteacher: curso.IdTeacher
-        };
-      });
-
-      // Asignar la primera URL a la variable urlSeleccionada
-      if (this.cursos.length > 0) {
-        const idTeacher = this.cursos[0].idteacher; // Obtener el ID del profesor
-        this.urlSeleccionada = this.cursos[0].url;
-        this.descripcionCurso = this.cursos[0].descripcion;
-
-        // Llamar a la función obtenerProfesor con el ID del profesor
-        this.obtenerProfesor(idTeacher);
-      }
-
-      console.log(this.cursos);
-    },
-    (error) => {
-      console.error(error);
-    }
-  );
-}
-
-getSafeVideoUrl(): SafeResourceUrl {
-  if (!this.urlSeleccionada) {
-    return ''; // O cualquier otro valor predeterminado en caso de que no haya URL seleccionada
-  }
-  const videoUrl = 'https://www.youtube.com/embed/' + this.urlSeleccionada;
-  console.log(videoUrl)
-
-  return this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
-}
-
-
-/*OBTENER PROFESOR*/
-
-
-obtenerProfesor(idTeacher: string) {
-  const url = `http://localhost:5000/api/teacher/${idTeacher}`;
-  this.http.get<any[]>(url).subscribe(
-    (response) => {
-      if (response.length > 0) {
-        const primerProfesor = response[0];
-        this.nombreProfeSeleccionado = primerProfesor.Name;
-        this.experienciaSeleccionada = primerProfesor.Experience;
-        this.profesionSeleccionada = primerProfesor.Profesion;
-      }
-
-      this.profesores = response;
-      console.log(this.profesores);
-    },
-    (error) => {
-      console.error(error);
-    }
-  );
-}
-      /*MANEJO BOTON CALIFICACIÓN*/
-      counters= [0,0,0,0,0];
-      qualification = 0;
-    
-      toggleQualification(index: number) {
-        if (this.counters[index] === 0) {
-          this.counters[index]++;
-          this.qualification++;
-        } else {
-          this.counters[index]--;
-          this.qualification--;
+        // Asignar la primera URL a la variable urlSeleccionada
+        if (this.cursos.length > 0) {
+          const idTeacher = this.cursos[0].idteacher; // Obtener el ID del profesor
+          this.urlSeleccionada = this.cursos[0].url;
+          this.descripcionCurso = this.cursos[0].descripcion;
+  
+          // Llamar a la función obtenerProfesor con el ID del profesor
+          this.obtenerProfesor(idTeacher);
         }
-      
+  
+        console.log(this.cursos);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  getSafeVideoUrl(): SafeResourceUrl {
+    if (!this.urlSeleccionada) {
+      return ''; // O cualquier otro valor predeterminado en caso de que no haya URL seleccionada
     }
-    categorias: any[] = [];
+    const videoUrl = 'https://www.youtube.com/embed/' + this.urlSeleccionada;
+    console.log(videoUrl)
+  
+    return this.sanitizer.bypassSecurityTrustResourceUrl(videoUrl);
+  }
+  obtenerProfesor(idTeacher: string) {
+    this.cursosService.obtenerProfesor(idTeacher).subscribe(
+      (response) => {
+        if (response.length > 0) {
+          const primerProfesor = response[0];
+          this.nombreProfeSeleccionado = primerProfesor.Name;
+          this.experienciaSeleccionada = primerProfesor.Experience;
+          this.profesionSeleccionada = primerProfesor.Profesion;
+        }
+  
+        this.profesores = response;
+        console.log(this.profesores);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+        /*MANEJO BOTON CALIFICACIÓN*/
+        counters= [0,0,0,0,0];
+        qualification = 0;
+      
+        toggleQualification(index: number) {
+          if (this.counters[index] === 0) {
+            this.counters[index]++;
+            this.qualification++;
+          } else {
+            this.counters[index]--;
+            this.qualification--;
+          }
+        
+      }
 
   ngOnInit(): void {
     this.obtenerCategorias();
   }
-
-
-
 }
--- Front-end/src/app/services/courses.service.ts
+++ Front-end/src/app/services/courses.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-  private apiUrl ='http://localhost:5000/api/'
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+  private apiUrl = 'http://localhost:5000/api'; // URL base de la API
 
-  
+  constructor(private http: HttpClient) {}
 
+  obtenerCategorias(): Observable<any[]> {
+    const url = `${this.apiUrl}/category`;
+    return this.http.get<any[]>(url);
+  }
+
+  obtenerCursos(idCategoria: string): Observable<any[]> {
+    const url = `${this.apiUrl}/course/?categoria=${idCategoria}`;
+    return this.http.get<any[]>(url);
+  }
+
+  obtenerProfesor(idProfesor: string): Observable<any[]> {
+    const url = `${this.apiUrl}/teacher/${idProfesor}`;
+    return this.http.get<any[]>(url);
+  }
 }
