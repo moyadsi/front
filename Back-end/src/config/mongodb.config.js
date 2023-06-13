e2934a0bf5cffd5310ad2c58e808ffc60833addf
“Diana <dmmoya3@misena.edu.co>
2023-06-13 17:55:38 -0500

    BackEnd Removido

--- Back-end/src/config/mongodb.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://Cohnante:3113408016@cluster0.elf78qi.mongodb.net/MetAnimation',{
-    useNewUrlParser: true,
-    UseUnifiedTopology: true,
-}).then(db=>console.log(`la base de datos mongodb se conecto`))
-    .catch(err => console.error(err));
